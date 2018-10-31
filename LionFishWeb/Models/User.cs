c3626b409a46f9b1dd73db8c97f09255d2796ccd
jyupiter <173098D@mymail.nyp.edu.sg>
2018-10-31 17:17:44 +0800

    8R8K

--- LionFishWeb/Models/User.cs
+++ LionFishWeb/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace LionFishWeb.Models
     public class User
     {
         public int UserId { get; set; }
+        [RegularExpression(@"^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", ErrorMessage = "E-mail is not valid")]
         public string Email { get; set; }
         public string PassHash { get; set; }
     }
